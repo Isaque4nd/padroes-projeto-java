af65c737bf32acda0e770f49c5c1a7ab70f0387b
Isaque <andradeisq1.7@gmail.com>
2024-10-25 00:21:56 -0300

    mudando package do singleton e mplementando o padrão de projeto comportamental Strategy

--- src/one/digitalinnovation/gof/App.java
+++ src/one/digitalinnovation/gof/App.java
@@ -1,7 +1,19 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        //Singleton
+        
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstancia();
@@ -16,5 +28,22 @@ public class App {
         System.out.println(holder);
         holder = SingletonLazyHolder.getInstancia();
         System.out.println(holder);
+
+        //Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressvo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressvo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
     }
 }
